e0b0998be263b97f625cfd416976f4ea6f888843
idkup <idkup137@gmail.com>
2021-04-20 16:36:50 -0400

    TourParticipant class, groundwork for TFTTour

--- TFTTour.py
+++ TFTTour.py
@@ -1,3 +1,9 @@
 class TFTTour:
-    def __init__(self):
-        pass
+    def __init__(self, name: str, channel_ids: list):
+        """Creates a new tournament object"""
+        self._name = name
+        self._log_channel_id = channel_ids[0]
+        self._results_channel_id = channel_ids[1]
+        self._scoring_channel_id = channel_ids[2]
+        self._chat_channel_id = channel_ids[3]
+        self._participants = []
--- TourParticipant.py
+++ TourParticipant.py
@@ -1,3 +1,39 @@
 class TourParticipant:
-    def __init__(self):
-        pass
+    def __init__(self, discord_id: int, name: str):
+        """Creates a new TourParticipant."""
+        self._discord_id = discord_id
+        self._name = name
+        self._score = 0
+        self._confirmed = False
+
+    def get_discord_id(self) -> int:
+        """Returns the participant's Discord ID."""
+        return self._discord_id
+
+    def get_name(self) -> str:
+        """Returns the participant's name."""
+        return self._name
+
+    def get_score(self) -> int:
+        """Returns the participant's current score."""
+        return self._score
+
+    def get_confirmed(self) -> bool:
+        """Returns if the participant has confirmed their participation."""
+        return self._confirmed
+
+    def set_discord_id(self, discord_id: int):
+        """Sets a new Discord ID for the participant."""
+        self._discord_id = discord_id
+
+    def set_name(self, name: str):
+        """Sets a new name for the participant."""
+        self._name = name
+
+    def set_score(self, score: int):
+        """Alters the participant's score."""
+        self._score = score
+
+    def set_confirmed(self, confirmed: bool):
+        """Sets the participant's confirmation status."""
+        self._confirmed = confirmed
